fdaf187f269e8b385b2f6fcc611d45f4dbd54e2c
EvilMoonBerry <petra.m.sjogren@gmail.com>
2025-07-11 11:03:58 +0300

    Added other test file. error handling and comments

--- reverse.c
+++ reverse.c
@@ -1,6 +1,5 @@
 //Project 1: Warmup to C and Unix programming
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,11 +16,10 @@ Node *create_node(const char *line_content){
         exit(1);
     }
 
-    // Allocate memory 
     size_t length = strlen(line_content) + 1;
     new_node->value = malloc(length);
     if (!new_node->value) {
-        perror("Failed to allocate memory");
+        fprintf(stderr,"error: malloc failed ");
         free(new_node);
         exit(1);
     }
@@ -31,17 +29,16 @@ Node *create_node(const char *line_content){
     return new_node;
 }
 
-// printing linked list in reverse 
+// print linked list in reverse 
 void printReverse(struct Node* curr) {
 
-    // Base case
     if (curr == NULL)
         return;
 
     // print the list after start node
     printReverse(curr->next);
 
-    // Check if the string ends with \n; if not, add one for correct printing
+    //Check if the string ends with \n; if not add one for the last lines for cleaner printing
     size_t len = strlen(curr->value);
     if (len == 0 || curr->value[len - 1] != '\n') {
         printf("%s\n", curr->value);
@@ -53,43 +50,68 @@ void printReverse(struct Node* curr) {
 void writeinfile(struct Node* curr) {
 
     FILE *fptr = fopen("testfile.txt", "a");
-
-    // Base case
+    
     if (curr == NULL)
         return;
 
-    // print the list after start node
     writeinfile(curr->next);
 
-    // Check if the string ends with \n; if not, add one for correct printing
-    size_t len = strlen(curr->value);
-    if (len == 0 || curr->value[len - 1] != '\n') {
-        fprintf(fptr,"%s\n", curr->value);
-        printf("%s\n", curr->value);
-    } else {
-        fprintf(fptr,"%s", curr->value);
-        printf("%s", curr->value);
-    }
+    fprintf(fptr,"%s", curr->value);
+
+    fclose(fptr);
 }
 
 int main (int argc, char *argv[]) {
 
-    // Open file for reading
-    FILE *fptr = fopen(argv[1],"r");
-    if (fptr ==NULL){
-        exit(1);
-    }
-
-    char *test =NULL;
+    FILE *fptr;
+    char *inputfile = NULL;
+    size_t input_len = 0;
+    char *part =NULL;
     size_t len =0;
     ssize_t line;
 
     Node *start = NULL;
     Node *end = NULL;
 
+    //Error if input and output files are the same
+    if (argc > 2 && strcmp(argv[1], argv[2]) == 0){
+        fprintf(stderr,"error: Input and output file must differ\n", argv[1]);
+        exit(1);
+    }
+
+    //Opening file from separate user input or from commandline
+    if (argc == 1) {
+        ssize_t chars_read = getline(&inputfile, &input_len, stdin);
+            if (chars_read == -1) {
+                fprintf(stderr, "Failed to read filename\n");
+                exit(1);
+            }
+
+            // Remove new line from user input for correct file name
+            inputfile[strcspn(inputfile, "\n")] = '\0';
+
+            //Open file, if the file cannot be open give error message
+            fptr = fopen(inputfile, "r");
+            if (fptr == NULL) {
+                fprintf(stderr, "error: cannot open file %s\n", inputfile);
+                free(inputfile);
+                exit(1);
+            }
+
+            // free inputfile
+            free(inputfile);
+        } else {
+            fptr = fopen(argv[1], "r");
+            if (fptr == NULL) {
+                fprintf(stderr, "error: cannot open file %s\n", argv[1]);
+                free(fptr);
+                exit(1);
+            }
+    }
+
     //Loop through file lines and store it to linked list
-    while ((line = getline(&test, &len, fptr)) != -1){
-        Node *new_node =  create_node (test);
+    while ((line = getline(&part, &len, fptr)) != -1){
+        Node *new_node =  create_node (part);
         if (!start){
             start = end = new_node;
         } else {
@@ -97,27 +119,19 @@ int main (int argc, char *argv[]) {
             end = new_node;
         }
     }
-    printf("\n");
-
-    // Close the file
+    
     fclose(fptr);
 
-    free(test);
+    free(part);
 
-    // printing the linked list
-    Node *printed_line = start;
-    while (printed_line){
-        printf("%s", printed_line->value);
-        printed_line = printed_line->next;
+    // If only input file was given then print the inputfile for user in reverse order
+    if (argc == 1 || argc == 2 ){
+        printReverse(start);
     }
-
-    printf("\n");
-
-    printf("\n");
-
-    printReverse(start);
-
-    writeinfile(start);
+    // If input and output file was given then write inputfile text to outputfile in reverse order
+    if(argc == 3){
+        writeinfile(start);
+    }
 
     return 0;
 
